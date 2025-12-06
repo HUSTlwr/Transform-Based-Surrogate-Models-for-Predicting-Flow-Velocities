d31939a9e742c2efa3366cc84402845f7659a76a
HUSTlwr <50614735+HUSTlwr@users.noreply.github.com>
2025-12-07 01:42:02 +0800

    Add files via upload

---
+++ Informer_Flowformer_Test_Verification/Flowformer/model/Informer.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.Transformer_EncDec import Decoder, DecoderLayer, Encoder, EncoderLayer, ConvLayer
+from layers.SelfAttention_Family import ProbAttention, AttentionLayer
+from layers.Embed import DataEmbedding
+
+
+class Model(nn.Module):
+    """
+    Informer with Propspare attention in O(LlogL) complexity
+    Paper link: https://ojs.aaai.org/index.php/AAAI/article/view/17325/17132
+    """
+
+    def __init__(self, configs):
+        super(Model, self).__init__()
+
+        self.pred_len = configs.pred_len
+        #self.label_len = configs.label_len
+
+        
+
+        if configs.channel_independence:
+            self.enc_in = 1
+            self.dec_in = 1
+            self.c_out = 1
+        else:
+            self.enc_in = configs.enc_in
+            self.dec_in = configs.dec_in
+            self.c_out = configs.c_out
+
+        # Embedding
+        self.enc_embedding = DataEmbedding(self.enc_in, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+        self.dec_embedding = DataEmbedding(self.dec_in, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        ProbAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=configs.output_attention),
+                        configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            [
+                ConvLayer(
+                    configs.d_model
+                ) for l in range(configs.e_layers - 1)
+            ] if configs.distil else None,
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AttentionLayer(
+                        ProbAttention(True, configs.factor, attention_dropout=configs.dropout, output_attention=False),
+                        configs.d_model, configs.n_heads),
+                    AttentionLayer(
+                        ProbAttention(False, configs.factor, attention_dropout=configs.dropout, output_attention=False),
+                        configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation,
+                )
+                for l in range(configs.d_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model),
+            projection=nn.Linear(configs.d_model, configs.c_out, bias=True)
+        )
+
+        self.projection = nn.Linear(configs.d_model, self.c_out)
+
+
+    def long_forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+
+        dec_out = self.decoder(dec_out, enc_out, x_mask=None, cross_mask=None)
+
+        return dec_out  # [B, L, D]
+
+
+    #def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+    #    dec_out = self.long_forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+    #    return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+
+
+    def forward(self, x_enc, x_mark_enc=None, x_dec=None, x_mark_dec=None, mask=None):
+        """
+        Encoder-only version of Informer forward pass.
+        Used when prediction length is 1 (pred_len=1) and decoder is unnecessary.
+        """
+
+        # -------- 1. Encoding --------
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)  # [B, seq_len, d_model]
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+
+        # -------- 2. Final Projection --------
+        # Only use the last time step's hidden state to make prediction
+        # enc_out[:, -1, :] --> [B, d_model]
+        # Project to output dimension: [B, c_out]
+        output = self.projection(enc_out[:, -1, :])  # self.projection = nn.Linear(d_model, c_out)
+
+        # -------- 3. Return shape --------
+        # Add singleton time dimension to match original Informer output shape: [B, pred_len=1, c_out]
+        return output.unsqueeze(1)
